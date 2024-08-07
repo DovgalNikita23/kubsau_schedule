a2907c10d8e70ea76d58e4987649ea01b837a1d8
Dovgal Nikita <dovgal@technokratos.com>
2024-08-07 13:43:04 +0300

    change cicd.yml

--- build/app/router/index.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { createBrowserRouter } from 'react-router-dom';
-export var router = createBrowserRouter([
-    {
-        path: '/',
-        element: _jsx("div", {}),
-    },
-]);
