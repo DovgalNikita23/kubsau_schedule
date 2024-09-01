f3f9c01e35acc3bc20f4014fc15efb1098909370
Nikita Dovgal <dovgal.nikita.anatolevich@gmal.com>
2024-09-01 14:48:21 +0300

    ui/added Pagination

--- src/shared/ui/Pagination/Pagination.tsx
+++ src/shared/ui/Pagination/Pagination.tsx
@@ -1,1 +1,14 @@
-export const Pagination = () => {}
+import './pagination.module.scss'
+import PaginationMUI, { PaginationProps } from '@mui/material/Pagination'
+import { FC } from 'react'
+import PaginationItem from '@mui/material/PaginationItem'
+
+export const Pagination: FC<PaginationProps> = (props: PaginationProps) => {
+  return (
+    <PaginationMUI
+      count={7}
+      {...props}
+      renderItem={(item) => <PaginationItem {...item}></PaginationItem>}
+    />
+  )
+}
